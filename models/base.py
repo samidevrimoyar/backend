6681f6d27849f59a970e5796b07f2470739499ac
samidevrimoyar <sami.devrim@gmail.com>
2025-07-23 21:09:50 +0300

    updated abes.py

--- models/base.py
+++ models/base.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from sqlalchemy import Column, DateTime
 from database import Base # database.py dosyasındaki Base'i kullanıyoruz
+from sqlalchemy.sql import func
 
 # Tüm modellerin miras alacağı temel model
 class BaseModel(Base):
     __abstract__ = True # Bu sınıf bir veritabanı tablosu olarak oluşturulmayacak
 
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
